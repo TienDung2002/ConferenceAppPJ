a7daa096de83f813773b5556c95fd73cf53302a9
TienDung2002 <101706065+TienDung2002@users.noreply.github.com>
2023-10-14 11:31:51 +0700

    update
    update

--- app/src/main/java/com/example/eventsconferencespj/Activities/Home/Home_Screen.kt
+++ app/src/main/java/com/example/eventsconferencespj/Activities/Home/Home_Screen.kt
@@ -18,17 +18,14 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.eventsconferencespj.Activities.Location
 import com.example.eventsconferencespj.Activities.Log.Login
-import com.example.eventsconferencespj.Fragments.Location.Location_Fragment
 import com.example.eventsconferencespj.R
 import com.example.eventsconferencespj.databinding.ActivityHomeScreenBinding
 import com.google.android.material.navigation.NavigationView
 
-class Home_Screen : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+//class Home_Screen : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class Home_Screen : AppCompatActivity(){
     private lateinit var binding: ActivityHomeScreenBinding
     private lateinit var viewModel: HomeScreenViewModel
-    private lateinit var drawerLayout : DrawerLayout
-    private lateinit var toogle : ActionBarDrawerToggle
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,81 +36,14 @@ class Home_Screen : AppCompatActivity(), NavigationView.OnNavigationItemSelected
         viewModel = ViewModelProvider(this).get(HomeScreenViewModel::class.java)
 
         // Đổi màu hint của search bar
-        var searchItem: SearchView? = binding.searchView
+        var searchItem: SearchView? = findViewById(R.id.search_view)
         if (searchItem != null) searchItem.setQueryHint(Html.fromHtml("<font color = #ffffff>" + "Tìm kiếm" + "</font>"))
-
-
-        val toolbar = binding.toolbar
-        val navigationView = binding.navView
-        drawerLayout = binding.drawerlayout
-        // toolbar
-        setSupportActionBar(toolbar)
-        // drawer menu
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-//        navigationView.bringToFront()
-        // Đăng ký sự kiện cho NavigationView
-        navigationView.setNavigationItemSelectedListener(this)
-
-        navigationView.setNavigationItemSelectedListener {
-            when (it.itemId){
-                R.id.nav_signout -> {
-                    val intent = Intent(this, Login::class.java)
-                    startActivity(intent)
-                    finish()
-                    Toast.makeText(this, "Bạn đã đăng xuất", Toast.LENGTH_SHORT).show()
-                }
-            }
-            true
-        }
 
 
         // Xử lý sự kiện khi nhấn vào nav_location
         binding.navLocation.setOnClickListener {
-//            viewModel.onNavLocationClicked()
             val intent = Intent(this, Location::class.java)
             startActivity(intent)
         }
-//        viewModel.openLocation.observe(this, Observer {
-//            // Tạo Fragment cho fragment_location
-//            val fragmentLocation = Location_Fragment()
-//            // Mở Fragment
-//            val transaction = supportFragmentManager.beginTransaction()
-//            transaction.replace(R.id.fragment_container_location, fragmentLocation)
-//            transaction.addToBackStack(null) // Để quay lại Fragment trước đó
-//            transaction.commit()
-//        })
-    }
-
-    // Điều hướng trong thanh menu
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-//                R.nav_home -> supportFragmentManager.beginTransaction()
-//                    .replace(R.id.fragment_container, HomeFragment()).commit()
-            R.id.nav_moreinf -> {
-                Toast.makeText(this, "Đang phát triển", Toast.LENGTH_SHORT).show()
-            }
-            R.id.nav_settings -> {
-                Toast.makeText(this, "Đang phát triển", Toast.LENGTH_SHORT).show()
-            }
-            R.id.nav_rateapp -> {
-                Toast.makeText(this, "Đang phát triển", Toast.LENGTH_SHORT).show()
-            }
-            R.id.nav_signout -> {
-                val intent = Intent(this, Login::class.java)
-                startActivity(intent)
-                finish()
-                Toast.makeText(this, "Bạn đã đăng xuất", Toast.LENGTH_SHORT).show()
-            }
-        }
-        // Đóng thanh drawer
-        binding.drawerlayout.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toogle.onOptionsItemSelected(item)) return true
-        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/eventsconferencespj/Activities/Users/User_Detail.kt
@@ -0,0 +1,12 @@
+package com.example.eventsconferencespj.Activities.Users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventsconferencespj.R
+
+class User_Detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_detail)
+    }
+}
